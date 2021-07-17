d814e51ce2f410de34cff37de0da1abd5d6c4372
Ollie Reardon <ojreardon@gmail.com>
2021-07-17 15:13:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Playtop
+A lightweight desktop for Linux-based handhelds written in Python 3.
+
+## Supported Devices
+* L4T Ubuntu for Switch
+* Ubuntu 20.04 x64
+
+## Running
+```
+chmod a+x run.sh
+./run.sh
+```
+
+## License
+MIT
---
+++ main.py
@@ -0,0 +1,49 @@
+import pygame, sys
+from pygame.locals import *
+
+#Set up pygame
+pygame.init()
+
+#Set up the window
+vw = pygame.display.Info().current_w;
+vh = pygame.display.Info().current_h;
+
+# windowSurface = pygame.display.set_mode((vw, vh), pygame.NOFRAME)
+windowSurface = pygame.display.set_mode((vw, vh))
+
+pygame.display.set_caption('Hello World')
+
+#Set up the colors
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+WHITE = (255,255,255)
+
+#Set up fonts
+basicFont = pygame.font.SysFont(None, 48)
+
+#Set up the text
+text = basicFont.render('HELLO WORLD', True, WHITE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
+
+#Draw the white background onto the surface
+windowSurface.fill(BLUE)
+
+#Draw a blue poligon onto the surface
+pygame.draw.rect(windowSurface, BLACK, pygame.Rect(0, 0, vw, 48));
+
+#Draw the text onto the surface
+windowSurface.blit(text,textRect)
+
+#Draw the window onto the screen
+pygame.display.update()
+
+#Run the game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
