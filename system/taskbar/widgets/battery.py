199d1610030b35caf3ca6066c0eb957361ec8ceb
Ollie Reardon <ojreardon@gmail.com>
2021-07-19 21:40:01 +0100

    Change low battery icon to red

--- system/taskbar/widgets/battery.py
+++ system/taskbar/widgets/battery.py
@@ -3,10 +3,11 @@ import pygame
 import psutil 
 
 class Battery:
-    def __init__(self, ws, x, y):
+    def __init__(self, ws, x, y, col = (255, 255, 255)):
         self.ws = ws
         self.x = x
         self.y = y
+        self.col = col
 
         battery = psutil.sensors_battery()
 
@@ -22,13 +23,14 @@ class Battery:
             self.status = fa.icons['battery-quarter']
         else:
             self.status = fa.icons['battery-empty']
+            self.col = (255, 0, 0)
         
     def draw(self):
         #Set up the text
         basicFont = pygame.font.Font("./assets/fonts/fontawesome-solid.otf", 24)
         print(psutil.sensors_battery());
         
-        text = basicFont.render(self.status, True, (255, 255, 255))
+        text = basicFont.render(self.status, True, self.col)
         textRect = text.get_rect()
         textRect.centerx = self.x
         textRect.centery = self.y
