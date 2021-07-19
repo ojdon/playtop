75ef1dbca2d047a0115f2c116b1afd215420537d
Ollie Reardon <ojreardon@gmail.com>
2021-07-19 23:34:37 +0100

    WIP: Menu work

--- system/menu/PlaytopMenu.py
+++ system/menu/PlaytopMenu.py
@@ -6,14 +6,18 @@ import xdg.DesktopEntry
 
 class PlaytopMenu:
     def show_menu(self, menu, depth=0):
+        menu_list = {}
+
         for entry in menu.getEntries():
             if isinstance(entry, xdg.Menu.Menu):
                 self.show_menu(entry, depth)
             elif isinstance(entry, xdg.Menu.MenuEntry):
-                print(menu.getPath() + "/	" + entry.DesktopFileID + "	" + entry.DesktopEntry.getFileName())
+                # print(menu.getPath() + "/	" + entry.DesktopFileID + "	" + entry.DesktopEntry.getFileName())
+                item = {"name": entry.DesktopEntry.getName(), "icon": entry.DesktopEntry.getIcon()}
+                menu_list.update(item)
+                print(menu_list)
+
+        return menu_list
 
     def get_menu(self):
         return xdg.Menu.parse()
-
-
-
